125be8820fb2926b14b31ac9fe4eec7f108ed170
Rachel Brindle <you@subluminal.net>
2016-10-19 17:15:36 -0700

    don't logger to stdout

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,9 +53,9 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  if ENV["RAILS_LOG_TO_STDOUT"].present?
-    config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
-  end
+  #if ENV["RAILS_LOG_TO_STDOUT"].present?
+    #config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  #end
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
